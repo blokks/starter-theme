0177b44ed3b5052d502ada5f8e2530214ef87a4e
Patrick Pietens <hello@patrickpietens.com>
2019-05-30 21:23:52 +0200

    Styling friends list

--- src/sw.blade.php
+++ src/sw.blade.php
@@ -10,16 +10,9 @@ const files = [
 	'{{ themes_url("$theme/modern.js") }}',
 	'{{ themes_url("$theme/legacy.js") }}',
 	'{{ themes_url("$theme/rows.css") }}',
-	'{{ themes_url("$theme/screen.css") }}',
+	'{{ themes_url("$theme/columns.css") }}',
+	'{{ themes_url("$theme/list.css") }}',
 	'{{ themes_url("$theme/icons.svg") }}',
-	'{{ themes_url("$theme/images/header.jpg") }}',
-	'{{ themes_url("$theme/images/esns-logo.png") }}',
-	'{{ themes_url("$theme/images/buma-logo.png") }}',
-	'{{ themes_url("$theme/images/oosterpoort-logo.png") }}',
-	'{{ themes_url("$theme/images/heineken-logo.png") }}',
-	'{{ themes_url("$theme/images/logo-europa-1.png") }}',
-	'{{ themes_url("$theme/images/logo-wildsea.png") }}',	
-	'https://fonts.googleapis.com/css?family=Montserrat:300,400|Roboto:400,500',
 ];
 
 enableOffline({ prefix, version, files, cdns: ['https://esns.nl/upload/media/'] });
